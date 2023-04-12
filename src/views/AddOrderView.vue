6ed74216cc8c2690c89af3e27493f4ded1d830a0
Maciej <szymanski.maciej1337@gmail.com>
2023-04-12 09:24:25 +0200

    Make simple Order View

---
+++ src/views/AddOrderView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <HeaderComponent :headerText="headerText" />
+    <OrderFormComponentVue />
+  </div>
+</template>
+<script>
+import HeaderComponent from "@/components/shared/HeaderComponent.vue";
+import OrderFormComponentVue from "@/components/addOrder/OrderFormComponent.vue";
+export default {
+  name: "AddOrderView",
+  components: {
+    HeaderComponent,
+    OrderFormComponentVue,
+  },
+  data() {
+    return {
+      headerText: "Formularz zamówienia",
+    };
+  },
+};
+</script>
+<style scoped></style>
