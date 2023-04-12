2d25761e887c31e1b53c99b0559dc1b25d26c6eb
Maciej <szymanski.maciej1337@gmail.com>
2023-04-12 19:59:34 +0200

    Add routing and new view

---
+++ src/views/ProductListView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <HeaderComponent :headerText="headerText" />
+    <PriceProductTable />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "@/components/shared/HeaderComponent.vue";
+import PriceProductTable from "@/components/mainScreen/PriceProductTable.vue";
+
+export default {
+  name: "ProductListView",
+  components: {
+    HeaderComponent,
+    PriceProductTable,
+  },
+  data() {
+    return {
+      headerText: "Lista produktów",
+    };
+  },
+};
+</script>
